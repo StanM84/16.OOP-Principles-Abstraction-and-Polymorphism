bdfb99646e18f522f751b55127fd71d4e5a88ca5
StanM84 <stan1984@abv.bg>
2023-12-07 15:19:15 +0200

    Stan
    exercise OOP Principle

---
+++ 16.OOP Principles Abstraction and Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Shapes;
+public class Circle : IDrawable
+{
+    private int radus;
+
+    public Circle (int radus)
+    {
+        this.radus = radus;
+    }
+
+    public void Draw()
+    {
+        double rIn = this.radus - 0.4;
+        double rOut = this.radus + 0.4;
+        for (double y = this.radus; y >= -this.radus; --y) 
+        {
+            for (double x = -this.radus; x < rOut; x += 0.5)
+            {
+                double value = x * x + y * y;
+
+                if (value >= rIn * rIn && value <= rOut * rOut) 
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
